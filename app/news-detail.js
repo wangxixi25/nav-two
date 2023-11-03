18f07fe3881071f4774f873539a907809ecadb62
widiya <widiyantiintandok25@gmail.com>
2023-11-03 07:33:00 +0700

    tambah tugas

--- app/news-detail.js
+++ app/news-detail.js
@@ -1,5 +1,27 @@
-import { Heading, Text, Center } from "@gluestack-ui/themed";
+// import { Heading, Text, Center } from "@gluestack-ui/themed";
+// import { Header } from "../components";
+// import { useLocalSearchParams } from "expo-router";
+
+// const NewsDetail = () => {
+//   const params = useLocalSearchParams();
+//   return (
+//     <>
+//       <Header title={"News"} withBack={true} />
+//       <Center flex={1} padding={"$4"}>
+//         <Heading>News Detail</Heading>
+//         <Text textAlign="center">{params.title}</Text>
+//       </Center>
+//     </>
+//   );
+// };
+
+// export default NewsDetail;
+
+// Tugas/
+
+import { Text, Image, Divider } from "@gluestack-ui/themed";
 import { Header } from "../components";
+import { ScrollView } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 
 const NewsDetail = () => {
@@ -7,10 +29,18 @@ const NewsDetail = () => {
   return (
     <>
       <Header title={"News"} withBack={true} />
-      <Center flex={1} padding={"$4"}>
-        <Heading>News Detail</Heading>
-        <Text textAlign="center">{params.title}</Text>
-      </Center>
+      <ScrollView Vertical={true} showsVerticalScrollIndicator={false}>
+      <Image
+          source={{ uri: params.image }}
+          w="100%"
+          h="20%"
+          alt="Image Data"
+        />
+        <Text padding={"$4"}>{params.date}</Text>
+        <Text padding={"$3"} fontSize={25} fontWeight="bold">{params.title}</Text>
+        <Divider bg="gray" h={1} my={1} marginHorizontal={10} />
+        <Text padding={"$4"} textAlign="justify">{params.content}</Text>
+        </ScrollView>
     </>
   );
 };
